02b739d5f3440b0d6ee0c579ee30859d2885add5
YFMARCO-Dev <qahralandrwyd4@gmail.com>
2024-10-21 20:03:30 +0000

    Add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+git clone --depth=1 https://github.com/YFMARCO-Dev/android_device_samsung_gta4l-common device/samsung/gta4l-common
+git clone --depth=1 https://github.com/YFMARCO-Dev/proprietary_vendor_samsung_gta4l-common vendor/samsung/gta4l-common
+git clone --depth=1 https://github.com/YFMARCO-Dev/proprietary_vendor_samsung_gta4l vendor/samsung/gta4l
+git clone --depth=1 https://github.com/YFMARCO-Dev/android_kernel_samsung_sm6115 kernel/samsung/sm6115
+git clone --depth=1 https://github.com/YFMARCO-Dev/android_hardware_samsung.git hardware/tmp
+cp -r hardware/tmp/* hardware/samsung/
+rm -rf hardware/tmp
+
+mkdir prebuilts/clang/host/linux-x86/clang-neutron; cd prebuilts/clang/host/linux-x86/clang-neutron;
+curl -LO "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman";
+chmod +x antman; ./antman -S=05012024; ./antman --patch=glibc; cd ../../../../../;sed -i '/CLANG_EXTRA_FLAGS += --hip-path=\/dev\/null/d' vendor/lineage/build/tasks/kernel.mk;
